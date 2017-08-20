6d364d813aa32a0e323b7ecf69c25b88ac37d5df
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2017-08-19 21:22:19 -0700

    Add player controls

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 (() => {
 'use strict';
 
-let maze;
+let maze, gridMaxY, gridMaxX, player;
+
+const Direction = {
+  NORTH: 0,
+  SOUTH: 1,
+  EAST: 2,
+  DENIS: 3,
+};
 
 const Element = {
   DIMENSIONS: document.querySelectorAll('.dimension'),
   GENERATE_BUTTON: document.getElementById('generate'),
   MAZE: document.querySelector('.maze'),
+  WIN: document.getElementById('win'),
+};
+
+const KeyCode = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
 };
 
 const PixelSize = {
@@ -141,23 +156,42 @@ function rand(n) {
 }
 
 function render(maze) {
+  // Convert maze grid to an unholy number of divs
   const grid = maze.asGrid();
-  const height = grid.length;
-  const width = grid[0].length;
-  const newMaze = createDiv(['maze']);
-  for (let y = 0; y < height; y++) {
-    const row = createDiv(['row', `y${y}`]);
-    for (let x = 0; x < width; x++) {
-      const cell = createDiv(['cell', `x${x}`, (grid[y][x] == TileType.WALL) ? 'wall' : 'open']);
+  gridMaxY = grid.length - 1;
+  gridMaxX = grid[0].length - 1;
+  const newMaze = createDiv('maze');
+  for (let y = 0; y <= gridMaxY; y++) {
+    const row = createDiv('row', `y${y}`);
+    for (let x = 0; x <= gridMaxX; x++) {
+      const cell = createDiv('cell', `x${x}`, (grid[y][x] == TileType.WALL) ? 'wall' : 'open');
       row.appendChild(cell);
     }
     newMaze.appendChild(row);
+  }
+  // Create an exit
+  const directionOfDeliverance = rand(4);
+  const randOddY = 2 * rand((gridMaxY) / 2) + 1;
+  const randOddX = 2 * rand((gridMaxX) / 2) + 1;
+  switch (directionOfDeliverance) {
+    case Direction.NORTH:
+      newMaze.firstChild.querySelector(`.x${randOddX}`).classList.remove('wall');
+      break;
+    case Direction.SOUTH:
+      newMaze.lastChild.querySelector(`.x${randOddX}`).classList.remove('wall');
+      break;
+    case Direction.EAST:
+      newMaze.querySelector(`.y${randOddY}`).lastChild.classList.remove('wall');
+      break;
+    case Direction.DENIS:
+      newMaze.querySelector(`.y${randOddY}`).firstChild.classList.remove('wall');
+      break;
   }
   Element.MAZE.parentNode.replaceChild(newMaze, Element.MAZE);
   Element.MAZE = newMaze;
 }
 
-function createDiv(classNames) {
+function createDiv(...classNames) {
   const div = document.createElement('div');
   for (let className of classNames) {
     div.classList.add(className);
@@ -177,6 +211,53 @@ function regenerate() {
   }
   maze = new Maze(height, width);
   render(maze);
+  initGame(); 
+}
+
+function initGame() {
+  const y = ((gridMaxY >> 2) << 1) + 1;
+  const x = ((gridMaxX >> 2) << 1) + 1;
+  const cell = Element.MAZE.querySelector(`.y${y} .x${x}`);
+  cell.classList.add('player');
+  player = {cell, y, x};
+  Element.WIN.classList.add('hidden');
+  document.addEventListener('keydown', movePlayer);
+}
+
+function movePlayer(event) {
+  let deltaY, deltaX;
+  switch (event.keyCode) {
+    case KeyCode.UP:
+      [deltaY, deltaX] = [-1, 0];
+      break;
+    case KeyCode.DOWN:
+      [deltaY, deltaX] = [1, 0];
+      break;
+    case KeyCode.LEFT:
+      [deltaY, deltaX] = [0, -1];
+      break;
+    case KeyCode.RIGHT:
+      [deltaY, deltaX] = [0, 1];
+      break;
+    default:
+      return;
+  }
+  let dest = [player.y + deltaY, player.x + deltaX];
+  let destCell = Element.MAZE.querySelector(`.y${dest[0]} .x${dest[1]}`);
+  if (destCell.classList.contains('wall')) {
+    return;
+  }
+  player.cell.classList.remove('player');
+  player = {
+    cell: destCell,
+    y: dest[0],
+    x: dest[1],
+  };
+  player.cell.classList.add('player');
+  if (dest[0] == 0 || dest[0] == gridMaxY || dest[1] == 0 || dest[1] == gridMaxX) {
+    document.removeEventListener('keydown', movePlayer);
+    Element.WIN.classList.remove('hidden');
+  }
 }
 
 Element.GENERATE_BUTTON.addEventListener('click', regenerate);
